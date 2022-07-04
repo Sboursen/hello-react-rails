1921ebd59c16299c8cc9557d86fdbe745f923406
sboursen <dev.sboursen@gmail.com>
2022-07-04 20:55:46 +0100

    Add basic style

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,24 +1,26 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getGreeting } from '../store/greetings';
 
-
-
 const Greeting = () => {
-    const dispatch = useDispatch();
-    const randomGreetingMessage = useSelector((state) => state.greeting.message);
+  const dispatch = useDispatch();
+  const randomGreeting = useSelector((state) => state.greeting);
 
-      useEffect(() => {
-        dispatch(getGreeting());
-      }, []);
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, []);
 
-    return <div>Message: {randomGreetingMessage}</div>;
-}
-
+  return (
+    <div className="box">
+      <h1 className="text">{randomGreeting.language}</h1>
+      <p className="text">{randomGreeting.message}</p>
+    </div>
+  );
+};
 
 Greeting.propTypes = {
-  message: PropTypes.string
+  message: PropTypes.string,
 };
-export default Greeting
+export default Greeting;
